f5bd72ea3703405cf3d5367e91bc00a70685a101
SaadSalman708 <saadsalman708@gmail.com>
2025-08-15 01:22:13 +0500

    adding js file

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 // Popup on Refresh
-alert("Please use CRL -(minus) or & CRL +(plus)to adjust screen size upto 67 % or depend on your's screen sizes")
+alert("Please use CRL -(minus) or & CRL +(plus) to adjust screen size upto 67 % or depend on your's screen sizes")
